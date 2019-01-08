de4755bb91bbcfd3aba8cd9e44d9f985cce00bb3
Simon <szymon.dzialowski@gmail.com>
2019-01-08 00:08:35 +0000

    console-logger

---
+++ express/console-logger.js
@@ -0,0 +1,14 @@
+/**
+ * app.use(require('nlab/express/console-logger'));
+ */
+module.exports = (req, res, next) => {
+    process.stdout.write(
+        (new Date()).toISOString().substring(0, 19).replace('T', ' ') +
+        ": controller " +
+        req.method.toUpperCase().padEnd(4, ' ') +
+        ":" +
+        req.url +
+        "\n"
+    );
+    next();
+};
