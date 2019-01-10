972cc0ba235a592e88eab0c895e6a17e454d1f8a
Simon <szymon.dzialowski@gmail.com>
2019-01-10 23:03:54 +0000

    md5

--- src/express/console-logger.js
+++ src/express/console-logger.js
@@ -3,16 +3,31 @@
  */
 module.exports = (req, res, next) => {
 
-    if ( ! res.clilogged ) {
+    process.stdout.write(
+        (new Date()).toISOString().substring(0, 19).replace('T', ' ') +
+        ": controller " +
+        req.method.toUpperCase().padEnd(4, ' ') +
+        ":" +
+        req.url +
+        "\n"
+    );
 
-        process.stdout.write(
-            (new Date()).toISOString().substring(0, 19).replace('T', ' ') +
-            ": controller " +
-            req.method.toUpperCase().padEnd(4, ' ') +
-            ":" +
-            req.url +
-            "\n"
-        );
-    }
     next();
 };
+
+/**
+ *
+ * Can be executed conditionally:
+ *
+    (function (logger) {
+        app.use((req, res, next) => {
+
+            if (req.url.indexOf('/public/') === 0 && req.url.split('.html').pop() !== '') { // ignore all public
+
+                return next();
+            }
+
+            return logger(req, res, next);
+        });
+    }(require('nlab/express/console-logger')));
+ */
