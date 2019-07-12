b640809999bca58ecd02b96e30b6f9578f0c513b
Szymon Dzialowski <stopsopa@users.noreply.github.com>
2019-07-12 16:24:48 +0100

    Update trim.js

--- src/trim.js
+++ src/trim.js
@@ -23,3 +23,20 @@ module.exports = function trim(string, charlist, direction) {
     (direction.indexOf('l')+1) && (string = string.replace(new RegExp('^['+charlist+']*(.*)$','gm'),'$1'));
     return string;
 }
+
+
+
+// /*!
+//  * @version 1.0 - 2013-05-21
+//  * @author Szymon Działowski
+//  * direction : 'rl'|'r'|'l'   -->   (undefined => 'rl')
+//  * charlist  : (undefined => " \n")
+//  */ 
+// function trim(string, charlist, direction) {
+//   direction = direction || 'rl';
+//   charlist  = (charlist || '').replace(/([.?*+^$[\]\\(){}|-])/g,'\\$1');
+//   charlist  = charlist || " \\n";
+//   (direction.indexOf('r')+1) && (string = string.replace(new RegExp('^(.*?)['+charlist+']*$','gm'),'$1'));
+//   (direction.indexOf('l')+1) && (string = string.replace(new RegExp('^['+charlist+']*(.*)$','gm'),'$1'));  
+//   return string;  
+// }   
