19cfeda720e057d9e7140adf045bbea633ed8482
stopsopa <grytysek@gmail.com>
2019-11-26 09:58:51 +0000

    old version of ms.js

--- src/ms.js
+++ src/ms.js
@@ -160,3 +160,38 @@ ms.generate = generate;
 
 module.exports = ms;
 
+/**
+ * Old version used in roderic, but it is limited in its functionality
+ */
+// module.exports = (function () {
+//     const pad = t => {
+//         return (t + '').length < 2 ? pad('0' + t + '') : t ;
+//     }
+//     return s => {
+//
+//         const d = Math.floor(s / (3600 * 24));
+//
+//         s  -= d * 3600 * 24;
+//
+//         const h   = Math.floor(s / 3600);
+//
+//         s  -= h * 3600;
+//
+//         const m = Math.floor(s / 60);
+//
+//         s  -= m * 60;
+//
+//         const tmp = [];
+//
+//         (d) && tmp.push(d + 'd');
+//
+//         (d || h) && tmp.push(h + 'h');
+//
+//         (d || h || m) && tmp.push(m + 'm');
+//
+//         tmp.push(s + 's');
+//
+//         return tmp.join(' ');
+//     }
+// }());
+
