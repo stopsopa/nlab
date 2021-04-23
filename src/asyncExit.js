eab54bad289d4998ca50a37eca5e584b02594df1
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2021-04-24 00:02:31 +0100

    asyncExit

---
+++ src/asyncExit.js
@@ -0,0 +1,97 @@
+
+function now() {
+  return (new Date()).toISOString().substring(0, 19).replace('T', ' ');
+}
+
+module.exports = async (promise, options) => {
+
+  let {
+    resume,
+    exitCodeNormal,
+    exitCodeTimeout,
+    promiseTimeoutMsec,
+    verbose,
+    bindTo,
+  } = Object.assign({}, {
+    resume              : true,
+    // https://nodejs.org/api/process.html#process_signal_events
+    bindTo              : ['exit', 'SIGINT', 'SIGTERM', 'SIGHUP', 'SIGBREAK', 'SIGUSR1', 'SIGUSR2', 'uncaughtException'],
+
+    exitCodeNormal      : 0,
+    exitCodeTimeout     : 1,
+    promiseTimeoutMsec  : 30000,
+    verbose             : true,
+    errors              : true,
+  }, options);
+
+  let stop = false;
+
+  async function onExit(origin) {
+
+    if (stop) {
+
+      return;
+    }
+
+    stop = true;
+
+    verbose && console.log(`${__filename} ${now()} start origin: ${origin}`);
+
+    onExit = () => {};
+
+    let timeoutHandler;
+
+    if (Number.isInteger(promiseTimeoutMsec)) {
+
+      timeoutHandler = setTimeout(() => {
+
+        errors && console.error(`${__filename} ${now()} promiseTimeoutMsec(${promiseTimeoutMsec})`);
+
+        process.exit(exitCodeTimeout);
+
+      }, promiseTimeoutMsec);
+    }
+
+    if (resume) {
+
+// https://stackoverflow.com/a/14032965/5560682
+// https://nodejs.org/api/process.html#process_signal_events
+      process.stdin.resume(); //so the program will not close instantly
+    }
+
+    try {
+
+      exitCodeNormal = parseInt(await promise(origin), 10);
+    }
+    catch (e) {
+
+      errors && console.error(`${__filename} ${now()} error: ${e}`);
+    }
+
+    clearTimeout(timeoutHandler);
+
+    verbose && console.log(`${__filename} ${now()} stop origin: ${origin}`);
+
+    process.exit(exitCodeNormal);
+  }
+
+  // // do something when app is closing
+  // process.on('exit', onExit.bind(null,'exit'));
+  //
+  // // catches ctrl+c event
+  // process.on('SIGINT', onExit.bind(null, 'SIGINT'));
+  //
+  // // catches "kill pid" (for example: nodemon restart)
+  // process.on('SIGUSR1', onExit.bind(null, 'SIGUSR1'));
+  // process.on('SIGUSR2', onExit.bind(null, 'SIGUSR2'));
+  //
+  // // catches uncaught exceptions
+  // process.on('uncaughtException', onExit.bind(null, 'uncaughtException'));
+
+  bindTo.forEach(event => {
+
+    // console.log(`bind ${event}`)
+
+    process.on(event, onExit.bind(null, event))
+  });
+}
