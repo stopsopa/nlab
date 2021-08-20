a5cc43edbece95e7207b98b94faeefbd9d7cf92f
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2021-08-20 12:40:19 +0100

    update

--- update-badge.js
+++ update-badge.js
@@ -26,7 +26,7 @@ let content = fs.readFileSync(README_md).toString();
 
 content = content.split("\n");
 
-let regnpm = /^\[!\[Build Status\]/;
+// let regnpm = /^\[!\[Build Status\]/;
 //let regcov = /^\[!\[Coverage Status\]/;
 let regcov = /^\[!\[codecov\]/;
 
@@ -36,7 +36,10 @@ let done = 0;
 
 for (let i = 0, l = content.length ; i < l ; i += 1 ) {
 
-    if (regnpm.test(content[i]) || regcov.test(content[i])) {
+    // if (regnpm.test(content[i]) || regcov.test(content[i])) {
+    if (regcov.test(content[i])) {
+
+        console.log('found: ' + content[i])
 
         content[i] = content[i].replace(/v\d+\.\d+\.\d+/g, n);
 
@@ -45,14 +48,14 @@ for (let i = 0, l = content.length ; i < l ; i += 1 ) {
             done += 1;
         }
 
-        if ( done === 2) {
+        if ( done === 1) {
 
             break;
         }
     }
 }
 
-if ( done !== 2 ) {
+if ( done !== 1 ) {
 
     throw `Badges not found, invalid badges or missing badges in file '${README_md}'`;
 
