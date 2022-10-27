23f1c0e926cc462510caedc9718ba62b5a6fd734
Szymon Dzialowski <stopsopa@users.noreply.github.com>
2022-10-27 23:26:32 +0100

    Update pregQuote.js

--- src/pregQuote.js
+++ src/pregQuote.js
@@ -5,6 +5,8 @@
  * @doc https://github.com/stopsopa/nlab#pregquote
  *
  * Logic based on https://www.npmjs.com/package/escape-string-regexp
+ *
+ * Compare later with: https://hapi.dev/module/hoek/api/?v=10.0.1#escaperegexstring
  */
 var matchOperatorsRe = /[|\\{}()[\]^$+*?.-]/g;
 
