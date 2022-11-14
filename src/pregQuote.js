182a6902db58eb9fb92b56fb394eff448190662f
Szymon Dzialowski <stopsopa@users.noreply.github.com>
2022-11-14 02:57:53 +0000

    Update pregQuote.js

--- src/pregQuote.js
+++ src/pregQuote.js
@@ -7,6 +7,7 @@
  * Logic based on https://www.npmjs.com/package/escape-string-regexp
  *
  * Compare later with: https://hapi.dev/module/hoek/api/?v=10.0.1#escaperegexstring
+ *                and: https://github.com/remix-run/react-router/blob/1ca0e8f6dbb4f02874183dadd6b2481a6d599008/packages/react-router-dom/modules/NavLink.js#L57
  */
 var matchOperatorsRe = /[|\\{}()[\]^$+*?.-]/g;
 
