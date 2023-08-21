6419a93f78b7010e7049e68db8c728164845b298
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2023-08-21 23:39:16 +0100

    refactor: fix ci

--- .github/pull-docker-image.sh
+++ .github/pull-docker-image.sh
@@ -2,6 +2,6 @@
 set -e
 set -x
 set -o pipefail
-VERSION="$(/bin/bash bash/swap-files-v2.sh package.json package.dev.json -- /bin/bash playwright.sh --version)"
+VERSION="$(SWAPQUIET=1 /bin/bash bash/swap-files-v2.sh package.json package.dev.json -- /bin/bash playwright.sh --version)"
 echo ">${VERSION}<"
 /bin/bash bash/swap-files-v2.sh package.json package.dev.json -- docker pull "mcr.microsoft.com/playwright:v${VERSION}-focal"   
