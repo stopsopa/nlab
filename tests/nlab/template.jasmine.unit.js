0eff5dc9ccc88974bd01bc993f69d101dd6a3ebf
Szymon Dzialowski <stopsopa@users.noreply.github.com>
2023-08-25 02:17:01 +0100

    refactor: fixing ci

--- tests/nlab/template.jasmine.unit.js
+++ tests/nlab/template.jasmine.unit.js
@@ -8,9 +8,9 @@ const template = require('../../template');
 const html = `
 <body>    
     <script src="../jasmine/bundles/jasmine.js"></script>
-<# for (let file of tests_list_paths) { #>  
-        <script src="<#= file #>"></script>
-<# } #>
+<% for (let file of tests_list_paths) { %>  
+        <script src="<%= file %>"></script>
+<% } %>
     <script src="../jasmine/bundles/node_modules/jasmine-core/lib/jasmine-core/boot1.js"></script>
 </body>
 </html>
