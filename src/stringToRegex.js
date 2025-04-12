2fc350a94c702eff070e7d2af1e8451100ce222c
Szymon Dzialowski <stopsopa@users.noreply.github.com>
2025-04-12 23:05:18 +0100

    Update stringToRegex.js q

--- src/stringToRegex.js
+++ src/stringToRegex.js
@@ -1,13 +1,20 @@
 const stringToRegex = (function () {
+  /**
+   * @param {string} msg
+   * @returns {Error}
+   */
   function th(msg) {
     return new Error("stringToRegex error: " + msg);
   }
 
+  /**
+   * @param {string} v
+   */
   return (v) => {
     try {
       const vv = v.match(/(\\.|[^/])+/g);
 
-      if (vv.length > 2) {
+      if (!vv || vv.length > 2) {
         throw new Error(`param '${v}' splits to more than 2 segments`);
       }
 
